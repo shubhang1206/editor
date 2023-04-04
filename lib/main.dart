b49f823812b9ff8e54cbf3ac4b317eaec446c6df
shubhang1206 <shubhang2002@gmail.com>
2023-04-04 13:16:54 +0530

    This is first commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_summernote/flutter_summernote.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Notes'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({required this.title});
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey<FlutterSummernoteState> _keyEditor = GlobalKey();
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: (){
+            },
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: FlutterSummernote(
+        hint: 'Your text here...',
+        key: _keyEditor,
+        hasAttachment: true,
+        customToolbar: """
+          [
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['insert', ['link', 'table', 'hr']]
+          ]
+        """,
+      ),
+    );
+  }
+}
